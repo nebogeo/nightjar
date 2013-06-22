4213e7cc5e0e6ad9763794da30a13fefca5a5f92
Dave Griffiths <dave@fo.am>
2013-06-22 17:15:11 +0100

    icon highlight js

---
+++ media/js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($)
+{
+    $("#bbsrc").hover(function () {
+        $(this).attr("src", "../media/images/bbsrc-l.png");
+    }, function () {
+        $(this).attr("src", "../media/images/bbsrc.png");
+    });
+
+    $("#exeter").hover(function () {
+        $(this).attr("src", "../media/images/exeter-l.png");
+    }, function () {
+        $(this).attr("src", "../media/images/exeter.png");
+    });
+
+    $("#sensory").hover(function () {
+        $(this).attr("src", "../media/images/sensory-ecology-l.png");
+    }, function () {
+        $(this).attr("src", "../media/images/sensory-ecology.png");
+    });
+
+});
