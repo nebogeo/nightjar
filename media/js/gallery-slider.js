54fd14edacc480353e4fcdb3e1470dbdd37de56d
Dave Griffiths <dave@fo.am>
2013-06-22 15:31:56 +0100

    gallery slider work

--- media/js/gallery-slider.js
+++ media/js/gallery-slider.js
@@ -21,8 +21,12 @@ jQuery(document).ready(function($)
     var last_x=0;
     var auto_scroll=true;
 
+    $(".arrow-left").mouseup(function(event) { auto_scroll=false; scroll_left(); });
+    $(".arrow-right").mouseup(function(event) { auto_scroll=false; scroll_right(); });
+    $(".arrow-left").hide();
+
     $(".slider-items").children().each(function(t,v) {
-        current_pos+=v.width;
+        current_pos+=v.clientWidth;
         image_positions.push(-current_pos);
     });
 
@@ -56,17 +60,38 @@ jQuery(document).ready(function($)
 
     $.timer(function () {
         if (auto_scroll) {
-            current_image++;
-            if (current_image>image_positions.length-2) current_image=0;
-
-            console.log(image_positions[current_image]);
-
-            $('.slider-items').animate({
-                left: ""+image_positions[current_image]+"px", top: "0px"
-            });
         }
     },
     6000,
     true);
+
+    var scroll_right = function() {
+        current_image++;
+        scroll();
+    };
+
+    var scroll_left = function() {
+        current_image--;
+        scroll();
+    };
+
+    var scroll = function() {
+        if (current_image>image_positions.length-2) current_image=0;
+        $('.slider-items').animate({
+            left: ""+image_positions[current_image]+"px", top: "0px"
+        });
+
+        $(".arrow-left").show();
+        $(".arrow-right").show();
+
+        if (current_image==0) {
+            $(".arrow-left").hide();
+        }
+        if (current_image==image_positions.length-2) {
+            $(".arrow-right").hide();
+        }
+    };
+
+
 
 });
