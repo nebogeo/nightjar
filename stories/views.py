9ac166698a3dea51eefb52e40e90afb08c87a49d
Dave Griffiths <dave@fo.am>
2013-07-08 15:24:34 +0100

    loadsa stylin stuff

--- stories/views.py
+++ stories/views.py
@@ -7,35 +7,47 @@ from stories.models import Story
 from stories.models import GalleryImage
 from blog.models import Post
 
-button_colours = ["#ff7777", "#77ff77", "#7777ff", "#00ffff", "#ff00ff"];
+import random
 
 def get_button_width(buttons,add):
-    button_width = 100
-    if len(buttons)>0: button_width=100/(len(buttons)+add)
-    button_width -= 2
+    button_width = 90
+    if len(buttons)>0: button_width=90/(len(buttons)+add)
     return button_width
 
-def hack_button_text_size(buttons):
-    c=0
+def get_size(str,size):
+    if len(str)>9:
+        if (size>150): size=150
+    if len(str)>13:
+        size=130
+    return size
+
+def calc_button_text_size(title, parent_title, buttons):
+    size=150
+#    for button in buttons:
+#        size=get_size(button.title,size)
+
+#    size=get_size(title,size)
+#    size=get_size(parent_title,size)
+    return size
+
+def random_button():
+    return "button-"+"%02d"%random.randrange(1,10)+".png";
+
+def calc_button_colours(buttons):
     for button in buttons:
-        button.colour=button_colours[c];
-        button.size=100;
-        if len(button.title)>12:
-            button.size=80;
-        if len(button.title)>16:
-            button.size=60;
-        c=c+1
+        button.image=random_button()
 
 def index(request):
     s = get_object_or_404(Story, id_name='main')
     template = loader.get_template('stories/index.html')
     buttons = Story.objects.filter(parent_id_name='main')
     posts = Post.objects.all().order_by('-date')[:5]
-    hack_button_text_size(buttons)
+    calc_button_colours(buttons)
     context = Context({
         'story': s,
         'buttons': buttons,
         'button_width': get_button_width(buttons,0),
+        'button_text_size': calc_button_text_size("","",buttons),
         'posts': posts,
         'leaf' : False
     })
@@ -45,27 +57,36 @@ def index(request):
 def story(request, story):
     s = get_object_or_404(Story, id_name=story)
     template = loader.get_template('stories/story.html')
+
     buttons = Story.objects.filter(parent_id_name=story)
+
     # if no buttons, display buttons on the same level
     leaf=False
     if len(buttons)==0:
         leaf=True
-        buttons = Story.objects.filter(Q(parent_id_name=s.parent_id_name) & ~Q(id_name=s.id_name))
-
+        buttons = Story.objects.filter(Q(parent_id_name=s.parent_id_name))
 
     gallery = GalleryImage.objects.filter(story__id_name=story)
+
     try:
         parent = Story.objects.get(id_name=s.parent_id_name)
     except Story.DoesNotExist:
         # no parents, must be index
         return redirect("/");
-    hack_button_text_size(buttons)
+
+    button_width = get_button_width(buttons,1);
+    if leaf: button_width=get_button_width(buttons,1);
+
+    calc_button_colours(buttons)
     context = Context({
         'story': s,
         'buttons': buttons,
-        'button_width': get_button_width(buttons,1),
+        'button_width': button_width,
+        'button_text_size': calc_button_text_size(s.title,parent.title,buttons),
         'gallery': gallery,
         'parent': parent,
+        'parent_button': random_button(),
+        'back_button': random_button(),
         'leaf': leaf
     })
     return HttpResponse(template.render(context))
