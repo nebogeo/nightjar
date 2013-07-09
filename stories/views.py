06e5dad280ce3b318e4cbf8e64f399010e2808c2
Dave Griffiths <dave@fo.am>
2013-07-09 11:29:16 +0100

    another load of layout changes

--- stories/views.py
+++ stories/views.py
@@ -10,9 +10,9 @@ from blog.models import Post
 import random
 
 def get_button_width(buttons,add):
-    button_width = 90
-    if len(buttons)>0: button_width=90/(len(buttons)+add)
-    return button_width-3
+    button_width = 100
+    if len(buttons)>0: button_width=100/(len(buttons)+add)
+    return button_width-2
 
 def get_size(str,size):
     if len(str)>10:
@@ -23,11 +23,11 @@ def get_size(str,size):
 
 def calc_button_text_size(title, parent_title, buttons):
     size=150
-    for button in buttons:
-        size=get_size(button.title,size)
+#    for button in buttons:
+#        size=get_size(button.title,size)
 
-    size=get_size(title,size)
-    size=get_size(parent_title,size)
+#    size=get_size(title,size)
+#    size=get_size(parent_title,size)
     return size
 
 def random_button():
@@ -37,16 +37,39 @@ def calc_button_colours(buttons):
     for button in buttons:
         button.image=random_button()
 
+def get_breadcrumbs(s):
+    done = False
+    crumbs = []
+    max_levels = 5
+    parent_id = s.parent_id_name
+    while not done and max_levels>0:
+        try:
+            current = Story.objects.get(id_name=parent_id)
+            crumbs.append(current)
+            max_levels=max_levels+1
+            parent_id = current.parent_id_name
+#            if parent_id == "main": done = True
+        except Story.DoesNotExist:
+            done = True
+    crumbs.reverse()
+    return crumbs
+
 def index(request):
     s = get_object_or_404(Story, id_name='main')
     template = loader.get_template('stories/index.html')
     buttons = Story.objects.filter(parent_id_name='main')
     posts = Post.objects.all().order_by('-date')[:5]
+
+    crumbs = [s]
+    calc_button_colours(crumbs)
     calc_button_colours(buttons)
+
     context = Context({
         'story': s,
         'buttons': buttons,
-        'button_width': get_button_width(buttons,0),
+        'crumbs': crumbs,
+        'button_width': get_button_width(crumbs,0),
+        'nav_button_width': get_button_width(buttons,0),
         'button_text_size': calc_button_text_size("","",buttons),
         'posts': posts,
         'leaf' : False
@@ -57,16 +80,12 @@ def index(request):
 def story(request, story):
     s = get_object_or_404(Story, id_name=story)
     template = loader.get_template('stories/story.html')
-
     buttons = Story.objects.filter(parent_id_name=story)
+    gallery = GalleryImage.objects.filter(story__id_name=story)
 
     # if no buttons, display buttons on the same level
-    leaf=False
     if len(buttons)==0:
-        leaf=True
         buttons = Story.objects.filter(Q(parent_id_name=s.parent_id_name))
-
-    gallery = GalleryImage.objects.filter(story__id_name=story)
 
     try:
         parent = Story.objects.get(id_name=s.parent_id_name)
@@ -74,19 +93,20 @@ def story(request, story):
         # no parents, must be index
         return redirect("/");
 
-    button_width = get_button_width(buttons,1);
-    if leaf: button_width=get_button_width(buttons,1);
-
+    crumbs = get_breadcrumbs(s)
+    calc_button_colours(crumbs)
     calc_button_colours(buttons)
+
     context = Context({
         'story': s,
         'buttons': buttons,
-        'button_width': button_width,
+        'crumbs': crumbs,
+        'button_width': get_button_width(crumbs,0),
+        'nav_button_width': get_button_width(buttons,0),
         'button_text_size': calc_button_text_size(s.title,parent.title,buttons),
         'gallery': gallery,
         'parent': parent,
         'parent_button': random_button(),
-        'back_button': random_button(),
-        'leaf': leaf
+        'back_button': random_button()
     })
     return HttpResponse(template.render(context))
